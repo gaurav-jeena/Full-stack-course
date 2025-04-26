0647b33e5075589ec103021812d5f4c99697a4ab
GAURAV SINGH JEENA <gauravjeena2002@gmail.com>
2025-04-26 21:40:33 +0530

    commit

---
+++ Javascript/intro.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+
+var myName = "Gaurav";
+var int = 10;
+// console.log(int);
+// console.log(typeof(int));
+console.log(myName);
+console.log(typeof +myName);
+console.log(true && myName);
+
+// var memories = null;
+// console.log(typeof(memories));
---
+++ Javascript/operator.js
@@ -0,0 +1,3 @@
+var score = 60;
+var result = score >=60 ? " student pass" : "student fail";
+console.log(result);
