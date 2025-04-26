c398c18903e31f610bff0a32d20c0d2490f7af1a
GAURAV SINGH JEENA <gauravjeena2002@gmail.com>
2025-04-26 23:02:36 +0530

    commit

--- Javascript/operator.js
+++ Javascript/operator.js
@@ -1,3 +1,25 @@
-var score = 60;
-var result = score >=60 ? " student pass" : "student fail";
-console.log(result);
+// var score = 60;
+// var result = score >=60 ? " student pass" : "student fail";
+// console.log(result);
+
+
+var age = 22;
+var citizen = false;
+var eligible = true;
+
+if(age>=18){
+    if(citizen){
+        if(eligible){
+            console.log("you are eligible to vote");
+        }
+        else{
+            console.log("you are not eligible due to registration");
+        }
+    }
+    else{
+        console.log("you are not eligible due to  citizenship");
+    }
+}
+else{
+    console.log("you are not eligible due to age");
+}
