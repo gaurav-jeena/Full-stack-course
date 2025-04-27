1be20bf829c6b199fc725534ef718bce08283ca7
GAURAV SINGH JEENA <gauravjeena2002@gmail.com>
2025-04-27 13:16:03 +0530

    commit

---
+++ Javascript/switch.js
@@ -0,0 +1,37 @@
+// var areaOfshape = "triangle"; // square, circle, rectangle
+// var a =5;
+// var b=10;
+// var result;
+
+// switch(areaOfshape){
+//     case "square":
+//         {
+//             result = a*a;
+//             console.log(areaOfshape + " area is " + result);
+//             break;  
+//         }
+//         case "circle":
+//             {
+//                 var r=2;
+//                 result = 3.14*r*r;
+//                 console.log(areaOfshape + " area is " + result);
+//                 break;
+//             }
+//         case "rectangle":
+//             {
+//                 result = a*b;
+//                 console.log(areaOfshape + " area is " + result);
+//                 break;
+//             }   
+//             default:
+//                 {
+//                     console.log("shape does not recognize");
+//                 }
+// }
+
+var sum = 0;
+for(var i =1; i<=10; i++){
+    sum+=i;
+    
+}
+console.log("sum of 10 numbers is " +sum);
