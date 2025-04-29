5082fbcda0cde3df2728d3d20adf40e24786fe5a
GAURAV SINGH JEENA <gauravjeena2002@gmail.com>
2025-04-29 17:00:59 +0530

    commit

--- Javascript/operator.js
+++ Javascript/operator.js
@@ -3,23 +3,46 @@
 // console.log(result);
 
 
-var age = 22;
-var citizen = false;
-var eligible = true;
+// var age = 22;
+// var citizen = false;
+// var eligible = true;
 
-if(age>=18){
-    if(citizen){
-        if(eligible){
-            console.log("you are eligible to vote");
-        }
-        else{
-            console.log("you are not eligible due to registration");
-        }
-    }
-    else{
-        console.log("you are not eligible due to  citizenship");
+// if(age>=18){
+//     if(citizen){
+//         if(eligible){
+//             console.log("you are eligible to vote");
+//         }
+//         else{
+//             console.log("you are not eligible due to registration");
+//         }
+//     }
+//     else{
+//         console.log("you are not eligible due to  citizenship");
+//     }
+// }
+// else{
+//     console.log("you are not eligible due to age");
+// }
+
+
+function calc(a,b,o)
+{
+    let result;
+    switch(o){
+        case '+':
+           return result=a+b;
+            break;
+        case'-':
+       return result=a-b;
+        break;
+        case'*':
+       return result=a*b;
+        break;
+
+        default:
+            return("invalid operator");
+
     }
 }
-else{
-    console.log("you are not eligible due to age");
-}
+
+console.log(calc(2,3,"*"));
